d2c303a7dd14da846dd0074cb825a03136c9fb43
Limug-g <dmlrbs94@gmail.com>
2026-08-04 17:22:23 +0900

    feature: Fix error code

--- practice01.js
+++ practice01.js
@@ -3,7 +3,7 @@
 // 요소 가져오기
 const todoInput = document.querySelector("#todoInput");
 const addBtn = document.querySelector("#addBtn");
-const filterBtn = document.querySelector(".filterBtn");
+const filterBtn = document.querySelectorAll(".filter-btn");
 const todoList = document.querySelector("#todoList");
 const emptyMessage = document.querySelector("#emptyMessage");
 
@@ -14,7 +14,7 @@ let idCounter = 1;
 
 //1. 할 일 추가
 function addTodo() {
-  const text = todoInput.ariaValueMax.trim();
+  const text = todoInput.value.trim();
   //trim()를 붙이는 이유는?
 
   if (text === "") {
@@ -37,16 +37,12 @@ function addTodo() {
 //2. filter()로 원하는 조건만 골라내기-> 콜백함수
 function getFilteredTodos() {
   if (currentFileter === "active") {
-    return todos.filter((todo) => {
-      todo.done === false;
-    });
+    return todos.filter((todo) => todo.done === false);
   }
   //'진행중' 필터를 클릭하면 보이는 할 일들(배열형태)
 
   if (currentFileter === "done") {
-    return todos.filter((todo) => {
-      todo.done === true;
-    });
+    return todos.filter((todo) => todo.done === true);
     //'완료됨' 필터를 클릭하면 보이는 할 일들
   }
   return todos;
@@ -83,7 +79,7 @@ function render() {
     span.textContent = todo.text;
 
     span.addEventListener("click", () => {
-      toggleTodo(todo, id);
+      toggleTodo(todo.id);
     });
 
     //할 일이 추가되면 삭제 버튼이 생성된 할 일 옆에 같이 만들어지는 작업
@@ -120,9 +116,7 @@ function toggleTodo(id) {
 //5. filter로 새 배열 만들기
 //이번엔 deleteTodo() 만들기
 function deleteTodo(id) {
-  todos = todos.filter((todo) => {
-    todo.id !== id;
-  });
+  todos = todos.filter((todo) => todo.id !== id);
   render();
 }
 
@@ -141,17 +135,41 @@ filterBtn.forEach((btn) => {
     filterBtn.forEach((b) => {
       //모든 버튼에서 active 클래서 제거
       b.classList.remove("active");
-
-      //클릭한 버튼만 active 클래서 추가
-      btn.classList.add("active");
-
-      currentFileter = btn.dataset.filter;
-      //맨 위에서 currentfileter를 all로 정의했음
-      //그래서 정확히 어떻게 돌아가는지는 모르겠네
-      //
-      render();
     });
+
+    //클릭한 버튼만 active 클래서 추가
+    btn.classList.add("active");
+
+    currentFileter = btn.dataset.filter;
+    //맨 위에서 currentfileter를 all로 정의했음
+    //그래서 정확히 어떻게 돌아가는지는 모르겠네
+    //
+    render();
   });
 });
 
 render();
+
+//에러 수정
+//1. filterBtn 클래스 이름 오류-> html에 있는대로 filter-btn으로 수정
+//2. map(), filter() 안에 {} 삭제-> () => 조건문 이렇게 수정
+//3. toffleTodo(todo, id) 파라미터 수정-> toggleTodo(todo.id) 로 수정
+//4. 마지막에 forEach() 안에 중괄호 영역 수정
+// filterBtn.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     filterBtn.forEach((b) => {
+//       //모든 버튼에서 active 클래서 제거
+//       b.classList.remove("active");
+
+//       //클릭한 버튼만 active 클래서 추가
+//       btn.classList.add("active");
+
+//       currentFileter = btn.dataset.filter;
+//       //맨 위에서 currentfileter를 all로 정의했음
+//       //그래서 정확히 어떻게 돌아가는지는 모르겠네
+//       //
+//       render();
+//     });
+//   });
+// });
+// -> 위에가 잘못된 표기
